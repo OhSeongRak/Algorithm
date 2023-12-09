2b0b01f02eecb6d65eeb65a9e49eb48d9470cd16
OhSeongRak <osr0907@gmail.com>
2023-12-09 16:05:23 +0900

    "pencil2" fix: 백준 암호코드 풀이 수정

--- dp/암호코드.py
+++ dp/암호코드.py
@@ -4,12 +4,22 @@ input = sys.stdin.readline
 
 
 def solution():
+    N = len(lst)
+    dp = [0] * N
+    dp[0], dp[1] = 1, 1
+
+    if lst[1] == 0:
+        return 0
+
+    for i in range(2, N):
+        if lst[i] != 0:  # lst[i]로만 숫자를 만들 수 있으므로 dp[i-1]만큼 가능
+            dp[i] += dp[i - 1]
+        if 10 <= 10 * lst[i - 1] + lst[i] <= 26:  # lst[i-1] lst[i]로 숫자를 만들 수 있으므로 dp[i-2]만큼 가능
+            dp[i] += dp[i - 2]
+
+    # return dp
+    return dp[-1] % 1000000
 
 
-    return
-
-
-N = int(input())
-lst = list(map(int, input().split()))
-K = int(input())
+lst = [-1] + list(map(int, input().strip()))
 print(solution())
