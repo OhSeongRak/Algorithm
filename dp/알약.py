261ab1ffd8161dad66451fd19c43aabd402d3e4e
OhSeongRak <osr0907@gmail.com>
2024-01-04 14:08:03 +0900

    :pencil: fix: 백준 4811 알약 bottom-up 풀이

--- dp/알약.py
+++ dp/알약.py
@@ -2,15 +2,19 @@ import sys
 
 input = sys.stdin.readline
 
-'''
-dp[0][6] = 0
-dp[1][5] = 1
-dp[1][4] = 
-dp[
-'''
-def solution():
 
-    return
+def solution():
+    dp = [[0 for _ in range((N + 1))] for _ in range(N + 1)]
+    dp[N][0] = 1
+
+    for w in range(N)[::-1]:
+        for h in range(N - w + 1)[::-1]:
+            if h + 1 <= N:
+                dp[w][h] += dp[w][h + 1]
+            if w + 1 <= N and h - 1 >= 0:
+                dp[w][h] += dp[w + 1][h - 1]
+
+    return dp[0][0]
 
 
 N = int(input())
