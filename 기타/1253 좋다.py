c3d7259d9d6121cdfd7f70e3d3f7e4e000fafd89
OhSeongRak <osr0907@gmail.com>
2024-03-21 23:55:31 +0900

    :sparkles: feat: 1253 좋다 풀이 완료

---
+++ 기타/1253 좋다.py
@@ -0,0 +1,28 @@
+import sys
+
+input = sys.stdin.readline
+
+
+def solution():
+    answer = 0
+
+    for i in range(N):
+        expt = lst[:i] + lst[i + 1:]
+        target = lst[i]
+        l, r = 0, N - 2
+        while l < r:
+            if expt[l] + expt[r] == target:
+                answer += 1
+                break
+            elif expt[l] + expt[r] < target:
+                l += 1
+            else:
+                r -= 1
+
+    return answer
+
+
+N = int(input())
+lst = list(map(int, input().split()))
+lst.sort()
+print(solution())
