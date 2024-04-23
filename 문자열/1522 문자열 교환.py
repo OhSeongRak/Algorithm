b5d9c1bca86d2d5f3623488382dc2f356a7cf758
OhSeongRak <osr0907@gmail.com>
2024-04-23 15:12:54 +0900

    :sparkles: feat: 1522 문자열 교환 풀이 완료

--- 문자열/1522 문자열 교환.py
+++ 문자열/1522 문자열 교환.py
@@ -1,53 +1,20 @@
 import sys
 
-from collections import deque
-
 input = sys.stdin.readline
 
 
 def solution():
-    if 'b' not in lst:
-        return 0
+    global lst
 
-    tmp1, tmp2 = lst.copy(), lst.copy()
-    while tmp1[-1] == 'a':
-        tmp1.appendleft(tmp1.pop())
+    total = lst.count('a')
+    answer = len(lst)
+    lst = lst + lst
 
-    while tmp2[0] == 'a':
-        tmp2.append(tmp2.popleft())
+    for i in range(len(lst) - total + 1):
+        answer = min(answer, total - lst[i:i + total].count('a'))
 
-    l, r = 0, len(lst) - 1
-    count1, count2 = 0, 0
-
-    while l < r:
-        if tmp1[l] == 'b' and tmp1[r] == 'a':
-            # lst[l], lst[r] = lst[r], lst[l]
-            count1 += 1
-            l += 1
-            r -= 1
-            continue
-
-        if tmp1[l] == 'a':
-            l += 1
-        if tmp1[r] == 'b':
-            r -= 1
-
-    l, r = 0, len(lst) - 1
-    while l < r:
-        if tmp2[l] == 'a' and tmp2[r] == 'b':
-            # lst[l], lst[r] = lst[r], lst[l]
-            count2 += 1
-            l += 1
-            r -= 1
-            continue
-
-        if tmp2[l] == 'b':
-            l += 1
-        if tmp2[r] == 'a':
-            r -= 1
-
-    return min(count1, count2)
+    return answer
 
 
-lst = deque(list(input().strip()))
+lst = list(input().strip())
 print(solution())
