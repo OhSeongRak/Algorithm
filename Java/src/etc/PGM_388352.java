98fdaa880e29542052fd91a7f3f8aa28b17867b6
OhSeongRak <osr0907@gmail.com>
2026-02-18 11:03:31 +0900

    2.18 비밀 코드 해독 ing

---
+++ Java/src/etc/PGM_388352.java
@@ -0,0 +1,16 @@
+package etc;
+
+public class PGM_388352 {
+    public static void main(String[] args) {
+        int n = 10;
+        int[][] q = {{1, 2, 3, 4, 5}, {6, 7, 8, 9, 10}, {3, 7, 8, 9, 10}, {2, 5, 7, 9, 10}, {3, 4, 5, 6, 7}};
+        int[] ans = {2, 3, 4, 3, 3};
+        System.out.println(solution(n, q, ans));
+    }
+
+    public static int solution(int n, int[][] q, int[] ans) {
+
+
+        return 0;
+    }
+}
